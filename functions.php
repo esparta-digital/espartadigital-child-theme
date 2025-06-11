8683e361830076e8361f96ecacb5a6c95f82ab24
Jose Manuel Fernández <38702514+jmfernxndez@users.noreply.github.com>
2025-06-11 08:00:25 +0200

    enqueue styles

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@
  * Funciones personalizadas para el child theme de Esparta Digital.
  */
 
+function esparta_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+}
+add_action( 'wp_enqueue_scripts', 'esparta_enqueue_styles' );
+
 // Desacopla estilos/scripts innecesarios de WordPress
 function espartadigital_optimize_assets() {
 	// Elimina estilos y scripts del theme padre
